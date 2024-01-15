2a35395040b2a3394b067a09d3d0221d8c73ecc6
iamwatchdogs <shamith3011022@gmail.com>
2024-01-16 03:49:42 +0530

    Configured dev dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,36 @@ const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-	mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: __dirname,
+			watch: true,
+        },
+        client: {
+            logging: 'info',
+			overlay: {
+				errors: true,
+				warnings: false,
+				runtimeErrors: true,
+			},
+        },
+        client: {
+            progress: true,
+        },
+        compress: true,
+        open: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+    },
+    devtool: 'source-map',
 });
