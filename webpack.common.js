6edd49cbf8c2a73a22be0f700dc078319f03cfb7
iamwatchdogs <shamith3011022@gmail.com>
2024-01-16 03:16:26 +0530

    Configured common dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: {
-		index: './src/script/index.js'
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].min.js'
-	}
+    entry: {
+        index: './src/scripts/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].min.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Meet Shamith',
+            template: './src/index.html',
+            filename: '../index.html',
+        }),
+    ],
 };
