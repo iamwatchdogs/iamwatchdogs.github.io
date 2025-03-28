d4fc6a29fd94d47947bbaaa0d9cb84db814a4468
iamwatchdogs <shamith301102@gmail.com>
2025-03-28 17:10:43 +0530

    configured prettier and precommit-hook

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,57 @@
 const js = require("@eslint/js");
 const globals = require("globals");
+const eslintConfigPrettier = require("eslint-config-prettier/flat");
+const prettierPlugin = require("eslint-plugin-prettier");
 
-module.exports = {
-  ...js.configs.recommended,
-  ignores: [
-    "**/*.min.js",
-    "dist/**",
-    "**/*.config.js",
-    "node_modules/**",
-    "package*.json",
-  ],
-  files: ["**/*.js", "**/*.json"],
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    globals: {
-      ...globals.browser,
-      ...globals.es2021,
-      ...globals.node,
+module.exports = [
+  {
+    ignores: [
+      "**/dist/**",
+      "**/config/**",
+      "**/node_modules/**",
+      "**/*.min.js",
+      "**/*.config.js",
+      "**/package*.json",
+      "package-lock.json",
+      ".prettierrc"
+    ]
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      }
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      "no-undef": "warn",
+      "no-unused-vars": "warn",
+      indent: ["error", 2],
+      semi: ["error", "always"],
     },
   },
-  rules: {
-    "no-undef": "off",
-    "no-unused-vars": "off",
-    indent: ["error", 2],
-    semi: ["error", "always"],
-  },
-};
+  {
+    files: ["**/webpack*.js", "**/config/*.js"],
+    languageOptions: {
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly"
+      },
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-undef": "off"
+    }
+  }
+];
